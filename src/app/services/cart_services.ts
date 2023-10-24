231a52ae4a30b0b07cb8141257b7bdac77c40352
Marlett <maalmarinba@ittepic.edu.mx>
2023-10-24 09:16:19 -0600

    Avance tab2

--- src/app/services/cart_services.ts
+++ src/app/services/cart_services.ts
@@ -1,17 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   private cartItems: any[] = [];
+  public cart : Product[] = [];
 
   getCartItems() {
     return this.cartItems;
   }
 
-  addToCart(item: any) {
-    this.cartItems.push(item);
+
+
+  addToCart(product: Product) {
+    const existingProduct = this.cartItems.find(item => item.name === product.name);
+
+    if (existingProduct) {
+      existingProduct.quantity++;
+    } else {
+      const productToAdd = { ...product };
+      productToAdd.quantity = 1;
+      this.cartItems.push(productToAdd);
+    }
+  }
+
+  removeToCart(product: Product) {
+    const existingProduct = this.cartItems.find(item => item.name === product.name);
+
+    if (existingProduct) {
+      const productToremove = { ...product };
+      productToremove.quantity = -1;
+      this.cartItems.push(productToremove);
+    } else {
+
+    }
   }
 
   getTotalPrice() {
